f81e8fdddac86229542ef9fc7d0ff6f56e230a67
mrclick <mr.click@goreclan.net>
2012-09-07 01:56:34 +0200

    added support for /rcon smite <player>

--- src/net/goreclan/console/Console.java
+++ src/net/goreclan/console/Console.java
@@ -12,7 +12,6 @@ package net.goreclan.console;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
