290e1a7458f4680ffecc8801da2a985970ac2ad3
Mathias Van Malderen <mvmalderen19@gmail.com>
2012-09-12 00:00:40 +0200

    fix javadoc

--- src/net/goreclan/event/EventType.java
+++ src/net/goreclan/event/EventType.java
@@ -1,33 +1,33 @@
 /**
  * This enum allows for type-safe comparisons when Event types are involved.
  * 
- * @author		Mathias Van Malderen
- * @version		1.0
- * @copyright	Mathias Van Malderen, 11 September, 2012
- * @package		net.goreclan.event
+ * @author      Mathias Van Malderen
+ * @version     1.0
+ * @copyright   Mathias Van Malderen, 11 September, 2012
+ * @package     net.goreclan.event
  **/
 
 package net.goreclan.event;
 
 public enum EventType {
-	
-	EVT_CLIENT_CALLVOTE,
-	EVT_CLIENT_CONNECT,
-	EVT_CLIENT_DISCONNECT,
-	EVT_CLIENT_GEAR_CHANGE,
-	EVT_CLIENT_HIT,
-	EVT_CLIENT_KILL,
-	EVT_CLIENT_NAME_CHANGE,
-	EVT_CLIENT_RADIO,
-	EVT_CLIENT_SAY,
-	EVT_CLIENT_SAY_PRIVATE,
-	EVT_CLIENT_SAY_TEAM,
-	EVT_CLIENT_SUICIDE,
-	EVT_CLIENT_TEAM_CHANGE,
-	EVT_CLIENT_VOTE,
-	EVT_GAME_EXIT,
-	EVT_GAME_ROUND_START,
-	EVT_GAME_WARMUP,
-	EVT_SURVIVOR_WINNER,
-	
+    
+    EVT_CLIENT_CALLVOTE,
+    EVT_CLIENT_CONNECT,
+    EVT_CLIENT_DISCONNECT,
+    EVT_CLIENT_GEAR_CHANGE,
+    EVT_CLIENT_HIT,
+    EVT_CLIENT_KILL,
+    EVT_CLIENT_NAME_CHANGE,
+    EVT_CLIENT_RADIO,
+    EVT_CLIENT_SAY,
+    EVT_CLIENT_SAY_PRIVATE,
+    EVT_CLIENT_SAY_TEAM,
+    EVT_CLIENT_SUICIDE,
+    EVT_CLIENT_TEAM_CHANGE,
+    EVT_CLIENT_VOTE,
+    EVT_GAME_EXIT,
+    EVT_GAME_ROUND_START,
+    EVT_GAME_WARMUP,
+    EVT_SURVIVOR_WINNER,
+    
 }
