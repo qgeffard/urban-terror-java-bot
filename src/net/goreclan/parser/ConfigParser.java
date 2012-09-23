3f75c65a9773cc2b5f3777e86edebf46a7642660
Mathias Van Malderen <mvmalderen19@gmail.com>
2012-09-23 14:35:01 +0200

    Fix indenting

--- src/net/goreclan/parser/ConfigParser.java
+++ src/net/goreclan/parser/ConfigParser.java
@@ -3,10 +3,10 @@
  * Every configuration is logically divided into configuration sections.
  * Each configuration section consists of multiple option settings.
  * 
- * @author		Mathias Van Malderen
- * @version 	1.0
- * @copyright 	Mathias Van Malderen 08 July, 2012
- * @package 	net.goreclan.utility.impl
+ * @author        Mathias Van Malderen
+ * @version     1.0
+ * @copyright     Mathias Van Malderen 08 July, 2012
+ * @package     net.goreclan.utility.impl
  **/
 
 package net.goreclan.parser;
@@ -16,85 +16,85 @@ import java.util.Map;
 import net.goreclan.exception.ParserException;
 
 public interface ConfigParser {
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Boolean.
-	 * 
-	 * @return boolean
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	boolean getBoolean(String section, String option) throws ParserException;
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Double.
-	 * 
-	 * @return double
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	double getDouble(String section, String option) throws ParserException;
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Float.
-	 * 
-	 * @return Float
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	float getFloat(String section, String option) throws ParserException;
-	
+    
+    
+    /**
+     * Read the option value of a configuration section as a Boolean.
+     * 
+     * @return boolean
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    boolean getBoolean(String section, String option) throws ParserException;
+    
+    
+    /**
+     * Read the option value of a configuration section as a Double.
+     * 
+     * @return double
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    double getDouble(String section, String option) throws ParserException;
+    
+    
+    /**
+     * Read the option value of a configuration section as a Float.
+     * 
+     * @return Float
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    float getFloat(String section, String option) throws ParserException;
+    
 
-	/**
-	 * Read the option value of a configuration section as an Integer.
-	 * 
-	 * @return int
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	int getInteger(String section, String option) throws ParserException;
+    /**
+     * Read the option value of a configuration section as an Integer.
+     * 
+     * @return int
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    int getInteger(String section, String option) throws ParserException;
 
-	
-	/**
-	 * Read the option value of a configuration section as a Long.
-	 * 
-	 * @return long
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	long getLong(String section, String option) throws ParserException;
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Short.
-	 * 
-	 * @return short
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	short getShort(String section, String option) throws ParserException;
-	
-	
-	/**
-	 * Read the option value of a configuration section as a String.
-	 * 
-	 * @return String
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	String getString(String section, String option) throws ParserException;
-	
-	
-	/**
-	 * Read all setting names and setting values from a section.
-	 * 
-	 * @return Map<String, String>
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	Map<String, String> getSettings(String section) throws ParserException;
-	
+    
+    /**
+     * Read the option value of a configuration section as a Long.
+     * 
+     * @return long
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    long getLong(String section, String option) throws ParserException;
+    
+    
+    /**
+     * Read the option value of a configuration section as a Short.
+     * 
+     * @return short
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    short getShort(String section, String option) throws ParserException;
+    
+    
+    /**
+     * Read the option value of a configuration section as a String.
+     * 
+     * @return String
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    String getString(String section, String option) throws ParserException;
+    
+    
+    /**
+     * Read all setting names and setting values from a section.
+     * 
+     * @return Map<String, String>
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    Map<String, String> getSettings(String section) throws ParserException;
+    
 }
--- src/net/goreclan/parser/XmlConfigParser.java
+++ src/net/goreclan/parser/XmlConfigParser.java
@@ -1,10 +1,10 @@
 /**
  * An XML Configuration parser for files.
  * 
- * @author		Mathias Van Malderen
- * @version 	1.0
- * @copyright 	Mathias Van Malderen 08 July, 2012
- * @package 	net.goreclan.utility.impl
+ * @author        Mathias Van Malderen
+ * @version     1.0
+ * @copyright     Mathias Van Malderen 08 July, 2012
+ * @package     net.goreclan.utility.impl
  **/
 
 package net.goreclan.parser;
@@ -30,210 +30,210 @@ import net.goreclan.exception.ConfigParserException;
 import net.goreclan.exception.ParserException;
 
 public class XmlConfigParser implements ConfigParser {
-	
-	private static final String SECTION_XPATH = "/configuration/settings[@name='%s']/set";
-	private static final String SETTING_XPATH = SECTION_XPATH + "[@name='%s']";
-	
-	private DocumentBuilder builder;
-	private Document document;
-	private XPath xpath;
-	
-	
-	/**
-	 * Class constructor.
-	 * 
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	public XmlConfigParser(String configPath) throws ParserException {
-		this(new File(configPath));		
-	}
-	
-	
-	/**
-	 * Class constructor.
-	 * 
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	public XmlConfigParser(File configPath) throws ParserException {
-		
-		try {
-			builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			xpath = XPathFactory.newInstance().newXPath();
-			document = builder.parse(configPath);
-		} catch (IOException | SAXException | ParserConfigurationException e) {
-			throw new ParserException("An error occured while initializing the XML parser.", e);
-		}
-		
-	}
-	
-	
-	/**
-	 * Return the option value lookup XPath expression with section and option filled in.
-	 * 
-	 * @return String
-	 * @author Mathias Van Malderen
-	 **/
-	private String getSettingXPath(String section, String option) {
-		return String.format(SETTING_XPATH, section, option);
-	}
-	
-	
-	/**
-	 * Return the section lookup XPath expression with section filled in.
-	 * 
-	 * @return String
-	 * @author Mathias Van Malderen
-	 **/
-	private String getSectionXPath(String section) {
-		return String.format(SECTION_XPATH, section);
-	}
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Boolean.
-	 * 
-	 * @return Boolean
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public boolean getBoolean(String section, String option) throws ParserException {
-		try {
-			return BooleanParser.valueOf(this.getString(section, option));
-		} catch (ParserException e) {
-			throw new ConfigParserException("The value cannot be parsed as a Boolean", section, option, e);
-		}
-	}
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Double.
-	 * 
-	 * @return Double
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public double getDouble(String section, String option) throws ParserException {
-		try {
-			return Double.valueOf(this.getString(section, option));
-		} catch (NumberFormatException e) {
-			throw new ConfigParserException("The value cannot be parsed as a Double.", section, option, e);
-		}
-	}
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Float.
-	 * 
-	 * @return Float
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public float getFloat(String section, String option) throws ParserException {
-		try {
-			return Float.valueOf(this.getString(section, option));
-		} catch (NumberFormatException e) {
-			throw new ConfigParserException("The value cannot be parsed as a Float.", section, option, e);
-		}
-	}
-	
-	
-	/**
-	 * Read the option value of a configuration section as an Integer.
-	 * 
-	 * @return Integer
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public int getInteger(String section, String option) throws ParserException {
-		try {
-			return Integer.valueOf(this.getString(section, option));
-		} catch (NumberFormatException e) {
-			throw new ConfigParserException("The value cannot be parsed as an Integer.", section, option, e);
-		}
-	}
-	
-	
+    
+    private static final String SECTION_XPATH = "/configuration/settings[@name='%s']/set";
+    private static final String SETTING_XPATH = SECTION_XPATH + "[@name='%s']";
+    
+    private DocumentBuilder builder;
+    private Document document;
+    private XPath xpath;
+    
+    
+    /**
+     * Class constructor.
+     * 
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    public XmlConfigParser(String configPath) throws ParserException {
+        this(new File(configPath));        
+    }
+    
+    
+    /**
+     * Class constructor.
+     * 
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    public XmlConfigParser(File configPath) throws ParserException {
+        
+        try {
+            builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            xpath = XPathFactory.newInstance().newXPath();
+            document = builder.parse(configPath);
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            throw new ParserException("An error occured while initializing the XML parser.", e);
+        }
+        
+    }
+    
+    
+    /**
+     * Return the option value lookup XPath expression with section and option filled in.
+     * 
+     * @return String
+     * @author Mathias Van Malderen
+     **/
+    private String getSettingXPath(String section, String option) {
+        return String.format(SETTING_XPATH, section, option);
+    }
+    
+    
+    /**
+     * Return the section lookup XPath expression with section filled in.
+     * 
+     * @return String
+     * @author Mathias Van Malderen
+     **/
+    private String getSectionXPath(String section) {
+        return String.format(SECTION_XPATH, section);
+    }
+    
+    
+    /**
+     * Read the option value of a configuration section as a Boolean.
+     * 
+     * @return Boolean
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public boolean getBoolean(String section, String option) throws ParserException {
+        try {
+            return BooleanParser.valueOf(this.getString(section, option));
+        } catch (ParserException e) {
+            throw new ConfigParserException("The value cannot be parsed as a Boolean", section, option, e);
+        }
+    }
+    
+    
+    /**
+     * Read the option value of a configuration section as a Double.
+     * 
+     * @return Double
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public double getDouble(String section, String option) throws ParserException {
+        try {
+            return Double.valueOf(this.getString(section, option));
+        } catch (NumberFormatException e) {
+            throw new ConfigParserException("The value cannot be parsed as a Double.", section, option, e);
+        }
+    }
+    
+    
+    /**
+     * Read the option value of a configuration section as a Float.
+     * 
+     * @return Float
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public float getFloat(String section, String option) throws ParserException {
+        try {
+            return Float.valueOf(this.getString(section, option));
+        } catch (NumberFormatException e) {
+            throw new ConfigParserException("The value cannot be parsed as a Float.", section, option, e);
+        }
+    }
+    
+    
+    /**
+     * Read the option value of a configuration section as an Integer.
+     * 
+     * @return Integer
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public int getInteger(String section, String option) throws ParserException {
+        try {
+            return Integer.valueOf(this.getString(section, option));
+        } catch (NumberFormatException e) {
+            throw new ConfigParserException("The value cannot be parsed as an Integer.", section, option, e);
+        }
+    }
+    
+    
 
-	/**
-	 * Read the option value of a configuration section as a Long.
-	 * 
-	 * @return Long
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public long getLong(String section, String option) throws ParserException {
-		try {
-			return Long.valueOf(this.getString(section, option));
-		} catch (NumberFormatException e) {
-			throw new ConfigParserException("The value cannot be parsed as a Long.", section, option, e);
-		}
-	}
-	
-	
-	/**
-	 * Read the option value of a configuration section as a Short.
-	 * 
-	 * @return Short
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public short getShort(String section, String option) throws ParserException {
-		try {
-			return Short.valueOf(this.getString(section, option));
-		} catch (NumberFormatException e) {
-			throw new ConfigParserException("The value cannot be parsed as a Short.", section, option, e);
-		}
-	}
-	
-	
-	/**
-	 * Read the option value of a configuration section as a String.
-	 * 
-	 * @return String
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public String getString(String section, String option) throws ParserException {
-		try {
-			String res = xpath.evaluate(getSettingXPath(section, option), document);
-			if (res.isEmpty()) throw new ConfigParserException("Configuration section or option not found.", section, option);
-			return res;
-		} catch (XPathExpressionException e) {
-			throw new ConfigParserException("An error occured while reading the option value.", section, option, e);
-		}
-	}
-	
-	
-	/**
-	 * Read all setting names and setting values from a section.
-	 * 
-	 * @return Map<String, String>
-	 * @author Mathias Van Malderen
-	 * @throws ParserException
-	 **/
-	@Override
-	public Map<String, String> getSettings(String section) throws ParserException {
-		try {
-			NodeList nodes = (NodeList) xpath.evaluate(getSectionXPath(section), document, XPathConstants.NODESET);
-			Map<String, String> res = new HashMap<String, String>();
-			for (int i = 0; i < nodes.getLength(); i++) {
-				String settingName  = nodes.item(i).getAttributes().getNamedItem("name").getNodeValue();
-				String settingValue = nodes.item(i).getChildNodes().item(0).getNodeValue();
-				res.put(settingName, settingValue);
-			}
-			return res;
-		} catch (XPathExpressionException e) {
-			throw new ConfigParserException("An error occured while reading a whole section.", section, null, e);
-		}
-	}
-	
+    /**
+     * Read the option value of a configuration section as a Long.
+     * 
+     * @return Long
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public long getLong(String section, String option) throws ParserException {
+        try {
+            return Long.valueOf(this.getString(section, option));
+        } catch (NumberFormatException e) {
+            throw new ConfigParserException("The value cannot be parsed as a Long.", section, option, e);
+        }
+    }
+    
+    
+    /**
+     * Read the option value of a configuration section as a Short.
+     * 
+     * @return Short
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public short getShort(String section, String option) throws ParserException {
+        try {
+            return Short.valueOf(this.getString(section, option));
+        } catch (NumberFormatException e) {
+            throw new ConfigParserException("The value cannot be parsed as a Short.", section, option, e);
+        }
+    }
+    
+    
+    /**
+     * Read the option value of a configuration section as a String.
+     * 
+     * @return String
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public String getString(String section, String option) throws ParserException {
+        try {
+            String res = xpath.evaluate(getSettingXPath(section, option), document);
+            if (res.isEmpty()) throw new ConfigParserException("Configuration section or option not found.", section, option);
+            return res;
+        } catch (XPathExpressionException e) {
+            throw new ConfigParserException("An error occured while reading the option value.", section, option, e);
+        }
+    }
+    
+    
+    /**
+     * Read all setting names and setting values from a section.
+     * 
+     * @return Map<String, String>
+     * @author Mathias Van Malderen
+     * @throws ParserException
+     **/
+    @Override
+    public Map<String, String> getSettings(String section) throws ParserException {
+        try {
+            NodeList nodes = (NodeList) xpath.evaluate(getSectionXPath(section), document, XPathConstants.NODESET);
+            Map<String, String> res = new HashMap<String, String>();
+            for (int i = 0; i < nodes.getLength(); i++) {
+                String settingName  = nodes.item(i).getAttributes().getNamedItem("name").getNodeValue();
+                String settingValue = nodes.item(i).getChildNodes().item(0).getNodeValue();
+                res.put(settingName, settingValue);
+            }
+            return res;
+        } catch (XPathExpressionException e) {
+            throw new ConfigParserException("An error occured while reading a whole section.", section, null, e);
+        }
+    }
+    
 }
