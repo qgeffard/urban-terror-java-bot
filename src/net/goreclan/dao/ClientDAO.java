d95de499a9c69c6369fe040822884b6ff1da9af5
Fenix986 <danielepantaleone@me.com>
2012-10-05 01:56:56 +0200

    - updated DAO interface (back to MySQL) + added new Exception handler

--- src/net/goreclan/dao/ClientDAO.java
+++ src/net/goreclan/dao/ClientDAO.java
@@ -2,8 +2,8 @@
  * This class represent a DAO Interface with the "clients" database table.
  *
  * @author      Daniele Pantaleone
- * @version     1.1
- * @copyright   Daniele Pantaleone, 7 September, 2012
+ * @version     1.2
+ * @copyright   Daniele Pantaleone, 05 October, 2012
  * @package     net.goreclan.dao
  **/
 
@@ -18,31 +18,32 @@ import java.util.Date;
 
 import net.goreclan.domain.Client;
 import net.goreclan.domain.Group;
+import net.goreclan.exception.RecordNotFoundException;
 import net.goreclan.utility.DataSourceManager;
 
 public class ClientDAO {
     
-	private static Connection conn;
-	private static PreparedStatement stmt;
-	private static ResultSet rs;
+	private static Connection connection;
+	private static PreparedStatement statement;
+	private static ResultSet resultset;
 	
-    private static final String LOAD = "SELECT `c`.`name` AS `c_name`, " +
-                                              "`c`.`guid` AS `c_guid`, " +
-                                              "`c`.`ip` AS `c_ip`, " +
-                                              "`c`.`connections` AS `c_connections`, " +
-                                              "`c`.`username` AS `c_username`, " +
-                                              "`c`.`password` AS `c_password`, " +
-                                              "`c`.`time_add` AS `c_time_add`, " +
-                                              "`c`.`time_edit` AS `c_time_edit`, " +
-                                              "`g`.`id` AS `g_id`, " +
-                                              "`g`.`name` AS `g_name`, " +
-                                              "`g`.`keyword` AS `g_keyword`, " +
-                                              "`g`.`level` AS `g_level` " +
-                                              "FROM `clients` AS `c` INNER JOIN `groups` AS `g` ON `c`.`group_id` = `g`.`id` WHERE `c`.`id` = ?";
+    private static final String LOAD = "SELECT `cl`.`name` AS `cl_name`, " +
+    										  "`cl`.`connections` AS `cl_connections`, " +
+                                              "`cl`.`ip` AS `cl_ip`, " +
+                                              "`cl`.`guid` AS `cl_guid`, " +
+                                              "`cl`.`auth` AS `cl_auth`, " +
+                                              "`cl`.`time_add` AS `cl_time_add`, " +
+                                              "`cl`.`time_edit` AS `cl_time_edit`, " +
+                                              "`gr`.`id` AS `gr_id`, " +
+                                              "`gr`.`name` AS `gr_name`, " +
+                                              "`gr`.`keyword` AS `gr_keyword`, " +
+                                              "`gr`.`level` AS `gr_level` " +
+                                              "FROM `clients` AS `cl` INNER JOIN `groups` AS `gr` " +
+                                              "ON `cl`.`group_id` = `gr`.`id` WHERE `cl`.`id` = ?";
     
-    public static final String INSERT = "INSERT INTO `clients` (`group_id`, `name`, `guid`, `ip`, `time_add`) VALUES (?,?,?,?,?)";
+    public static final String INSERT = "INSERT INTO `clients` (`group_id`, `name`, `ip`, `guid`, `auth`, `time_add`) VALUES (?,?,?,?,?,?)";
     
-    public static final String UPDATE = "UPDATE `clients` SET `group_id` = ?, `name` = ?, `guid` = ?, `ip` = ?, `connections` = ?, `username` = ?, `password` = ?, `time_add` = ?, `time_edit`= ? WHERE `id` = ?";
+    public static final String UPDATE = "UPDATE `clients` SET `group_id` = ?, `name` = ?, `connections` = ?, `ip` = ?, `guid` = ?, `auth` = ?, `time_add` = ?, `time_edit`= ? WHERE `id` = ?";
     
     public static final String DELETE = "DELETE FROM `clients` WHERE `id` = ?";
     
@@ -51,43 +52,42 @@ public class ClientDAO {
      * Load values from the database and fill object attributes.
      * 
      * @author Daniele Pantaleone
+     * @param  client The client object where to store data
      * @throws ClassNotFoundException 
      * @throws SQLException 
+     * @throws RecordNotFoundException 
      **/
-    public static void load(Client client) throws ClassNotFoundException, SQLException {
+    public static void load(Client client) throws ClassNotFoundException, SQLException, RecordNotFoundException {
         
-    	if (conn == null || conn.isClosed()) 
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 
+    		connection = DataSourceManager.getConnection();
         
-    	stmt = conn.prepareStatement(LOAD);
-        stmt.setInt(1, client.id);
-        rs = stmt.executeQuery();
+    	statement = connection.prepareStatement(LOAD);
+    	statement.setInt(1, client.id);
+        resultset = statement.executeQuery();
         
-        if (rs.next()) {
+        if (!resultset.next())
+        	throw new RecordNotFoundException("Unable to find a match for client: @" + client.id + ".");
         
-            // Creating the Group object
-            Group group = new Group();
-            group.id = rs.getInt("g_id");
-            group.name = rs.getString("g_name");
-            group.keyword = rs.getString("g_keyword");
-            group.level = rs.getInt("g_level");
+        // Creating the Group object
+        Group group = new Group();
+        group.id = resultset.getInt("gr_id");
+        group.name = resultset.getString("gr_name");
+        group.keyword = resultset.getString("gr_keyword");
+        group.level = resultset.getInt("gr_level");
+        
+        // Storing the client data
+        client.group = group;
+        client.name = resultset.getString("cl_name");
+        client.connections = resultset.getInt("cl_connections");
+        client.ip = resultset.getString("cl_ip");
+        client.guid = resultset.getString("cl_guid");
+        client.auth = resultset.getString("cl_auth");
+        client.time_add = new Date(resultset.getLong("cl_time_add"));
+        client.time_edit = new Date(resultset.getLong("cl_time_edit"));
             
-            // Filling object attributes
-            client.group = group;
-            client.name = rs.getString("c_name");
-            client.guid = rs.getString("c_guid");
-            client.ip = rs.getString("c_ip");
-            client.connections = rs.getInt("c_connections");
-            client.username = rs.getString("c_username");
-            client.password = rs.getString("c_password");
-            client.time_add = new Date(rs.getLong("c_time_add"));
-            client.time_edit = new Date(rs.getLong("c_time_edit"));
-            
-        }
-        
-        // Closing current ResultSet and Prepared Statement
-        if (rs != null) rs.close();
-        if (stmt != null) stmt.close();
+        resultset.close();
+        statement.close();
             
     }
     
@@ -96,29 +96,30 @@ public class ClientDAO {
      * Create a new entry in the database for the current object.
      * 
      * @author Daniele Pantaleone
+     * @param  client The client whose informations needs to be stored
      * @throws ClassNotFoundException 
      * @throws SQLException 
      **/
     public static void insert(Client client) throws ClassNotFoundException, SQLException { 
         
-    	if (conn == null || conn.isClosed()) 
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 
+    		connection = DataSourceManager.getConnection();
     	
-    	stmt = conn.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
-    	stmt.setInt(1, client.group.id);
-        stmt.setString(2, client.name);
-        stmt.setString(3, client.guid);
-        stmt.setString(4, client.ip);
-        stmt.setLong(5, client.time_add.getTime());
-        stmt.executeUpdate();
+    	statement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+    	statement.setInt(1, client.group.id);
+    	statement.setString(2, client.name);
+    	statement.setString(3, client.ip);
+    	statement.setString(4, client.guid);
+    	statement.setString(5, client.auth);
+    	statement.setLong(6, client.time_add.getTime());
+    	statement.executeUpdate();
     	
-        // Getting the auto-incremented client id
-    	rs = stmt.getGeneratedKeys();
-    	client.id = rs.getInt(1);
+    	// Storing the new generated client id
+    	resultset = statement.getGeneratedKeys();
+    	client.id = resultset.getInt(1);
         
-    	// Closing current ResultSet and Prepared Statement
-        if (rs != null) rs.close();
-        if (stmt != null) stmt.close();
+        resultset.close();
+        statement.close();
         
     }
     
@@ -127,29 +128,27 @@ public class ClientDAO {
      * Update domain object in the database.
      * 
      * @author Daniele Pantaleone
+     * @param  client The client whose informations needs to be updated
      * @throws ClassNotFoundException 
      * @throws SQLException 
      **/
     public static void update(Client client) throws ClassNotFoundException, SQLException { 
         
-    	if (conn == null || conn.isClosed()) 
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 
+    		connection = DataSourceManager.getConnection();
         
-    	stmt = conn.prepareStatement(UPDATE);
-        stmt.setInt(1, client.group.id);
-        stmt.setString(2, client.name);
-        stmt.setString(3, client.guid);
-        stmt.setString(4, client.ip);
-        stmt.setInt(5, client.connections);
-        stmt.setString(6, client.username);
-        stmt.setString(7, client.password);
-        stmt.setLong(8, client.time_add.getTime());
-        stmt.setLong(9, client.time_edit.getTime());
-        stmt.setInt(10, client.id);
-        stmt.executeUpdate();
-        
-        // Closing current Prepared Statement
-        if (stmt != null) stmt.close();
+    	statement = connection.prepareStatement(UPDATE);
+    	statement.setInt(1, client.group.id);
+    	statement.setString(2, client.name);
+    	statement.setInt(3, client.connections);
+    	statement.setString(4, client.ip);
+    	statement.setString(5, client.guid);
+    	statement.setString(6, client.auth);
+    	statement.setLong(7, client.time_add.getTime());
+        statement.setLong(8, client.time_edit.getTime());
+        statement.setInt(9, client.id);
+        statement.executeUpdate();
+        statement.close();
         
     }
     
@@ -158,20 +157,19 @@ public class ClientDAO {
      * Delete domain object from the database.
      * 
      * @author Daniele Pantaleone
+     * @param  client The client whose informations needs to be deleted
      * @throws ClassNotFoundException 
      * @throws SQLException 
      **/
     public static void delete(Client client) throws ClassNotFoundException, SQLException { 
         
-    	if (conn == null || conn.isClosed()) 
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 
+    		connection = DataSourceManager.getConnection();
         
-        stmt = conn.prepareStatement(DELETE);
-        stmt.setInt(1, client.id);
-        stmt.executeUpdate();
-        
-        // Closing current Prepared Statement
-        if (stmt != null) stmt.close();
+    	statement = connection.prepareStatement(DELETE);
+    	statement.setInt(1, client.id);
+    	statement.executeUpdate();
+        statement.close();
         
     }
     
