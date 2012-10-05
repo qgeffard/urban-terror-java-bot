cf6816e129367071d7a3daaaa5ddf623a9e70c9a
Fenix986 <danielepantaleone@me.com>
2012-10-05 16:26:28 +0200

    - added minor description

--- src/net/goreclan/dao/ClientDAO.java
+++ src/net/goreclan/dao/ClientDAO.java
@@ -176,6 +176,8 @@ public class ClientDAO {
         
     	statement = connection.prepareStatement(DELETE);
     	statement.setInt(1, client.id);
+    	
+    	// Executing the statement.
     	statement.executeUpdate();
         statement.close();
         
