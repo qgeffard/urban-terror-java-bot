e25a8a31e5c25406b643e5390b5b46c00fff1e6f
Fenix986 <danielepantaleone@me.com>
2012-10-05 18:31:57 +0200

    - added QColors class to handle Quake3 color codes

---
+++ src/net/goreclan/iourt42/QColors.java
@@ -0,0 +1,23 @@
+/**
+ * This class declare the Quake3 color codes.
+ * 
+ * @author      Daniele Pantaleone
+ * @version     1.0
+ * @copyright   Daniele Pantaleone, 05 October, 2012
+ * @package     net.goreclan.iourt42
+ **/
+
+package net.goreclan.iourt42;
+
+public class QColors {
+	
+	public static final String RED = "^1";
+	public static final String GREEN = "^2";
+	public static final String YELLOW = "^3";
+	public static final String BLUE = "^4";
+	public static final String CYAN = "^5";
+	public static final String MAGENTA = "^6";
+	public static final String WHITE = "^7";
+	public static final String BLACK = "^8";
+
+}
