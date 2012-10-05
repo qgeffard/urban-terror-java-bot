5bf414ef26e4f4ce60f1a50a7ab6da670acb6a55
Fenix986 <danielepantaleone@me.com>
2012-10-05 13:18:25 +0200

    - updated DAO interface

--- src/net/goreclan/dao/AliasDAO.java
+++ src/net/goreclan/dao/AliasDAO.java
@@ -1,10 +1,10 @@
 /**
  * This class represents a DAO interface with the "aliases" database table.
  * 
- * @author Mathias Van Malderen, Daniele Pantaleone
- * @version 1.1
- * @copyright Mathias Van Malderen, Daniele Pantaleone, 30 June, 2012
- * @package net.goreclan.dao
+ * @author 		Mathias Van Malderen, Daniele Pantaleone
+ * @version 	1.2
+ * @copyright 	Mathias Van Malderen, 05 October, 2012
+ * @package 	net.goreclan.dao
  **/
 
 package net.goreclan.dao;
@@ -17,20 +17,16 @@ import java.sql.Statement;
 import java.util.Date;
 
 import net.goreclan.domain.Alias;
+import net.goreclan.exception.RecordNotFoundException;
 import net.goreclan.utility.DataSourceManager;
 
 public class AliasDAO {
 	
-	private static Connection conn;
-	private static PreparedStatement stmt;
-	private static ResultSet rs;
+	private static Connection connection;
+	private static PreparedStatement statement;
+	private static ResultSet resultset;
 	
-    private static final String LOAD = "SELECT `a`.`client_id` AS `a_client_id`," +
-                                              "`a`.`name` AS `a_name`," +
-                                              "`a`.`num_used` AS `a_num_used`," +
-                                              "`a`.`time_add` AS `a_time_add`," +
-                                              "`a`.`time_edit` AS `a_time_edit`" +
-                                              "FROM `aliases` AS `a` WHERE `a`.`id` = ?";
+    private static final String LOAD = "SELECT `client_id`, `name`, `num_used`, `time_add`, `time_edit` FROM `aliases` WHERE `id` = ?";
     
     private static final String INSERT = "INSERT INTO `aliases` (`client_id`, `name`, `num_used`, `time_add`) VALUES (?,?,?,?)";
     
@@ -43,32 +39,32 @@ public class AliasDAO {
      * Load values from the database and fill object attributes.
      * 
      * @author Mathias Van Malderen, Daniele Pantaleone
+     * @param  alias The alias object where to store data
      * @throws ClassNotFoundException 
      * @throws SQLException 
+     * @throws RecordNotFoundException 
      **/
-    public static void load(Alias alias) throws ClassNotFoundException, SQLException {
+    public static void load(Alias alias) throws ClassNotFoundException, SQLException, RecordNotFoundException {
         
-    	if (conn == null || conn.isClosed()) 
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 
+    		connection = DataSourceManager.getConnection();
         
-    	stmt = conn.prepareStatement(LOAD);
-        stmt.setInt(1, alias.id);
-        rs = stmt.executeQuery();
+    	statement = connection.prepareStatement(LOAD);
+    	statement.setInt(1, alias.id);
+    	resultset = statement.executeQuery();
     	
-        if (rs.next()) {
-        
-        	// Filling object attributes
-        	alias.client_id = rs.getInt("a_client_id");
-        	alias.name = rs.getString("a_name");
-        	alias.num_used = rs.getInt("a_num_used");
-        	alias.time_add = new Date(rs.getLong("a_time_add"));
-        	alias.time_edit = new Date(rs.getLong("a_time_edit"));
-        
-        }
-        
-        // Closing current ResultSet and Prepared Statement
-        if (rs != null) rs.close();
-        if (stmt != null) stmt.close();
+    	if (!resultset.next())
+        	throw new RecordNotFoundException("Unable to find a match for alias: " + alias.id + ".");
+    	
+    	// Storing the alias data
+    	alias.client_id = resultset.getInt("client_id");
+    	alias.name = resultset.getString("name");
+    	alias.num_used = resultset.getInt("num_used");
+    	alias.time_add = new Date(resultset.getLong("time_add"));
+    	alias.time_edit = new Date(resultset.getLong("time_edit"));
+
+    	resultset.close();
+        statement.close();
             
     }
     
@@ -77,28 +73,28 @@ public class AliasDAO {
      * Create a new entry in the database for the current object.
      * 
      * @author Mathias Van Malderen, Daniele Pantaleone
+     * @param  alias The alias whose informations needs to be stored
      * @throws ClassNotFoundException 
      * @throws SQLException 
      **/
     public static void insert(Alias alias) throws ClassNotFoundException, SQLException {
         
-    	if (conn == null || conn.isClosed()) 
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 
+    		connection = DataSourceManager.getConnection();
     	
-    	stmt = conn.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
-    	stmt.setInt(1, alias.client_id);
-    	stmt.setString(2, alias.name);
-    	stmt.setInt(3, alias.num_used);
-    	stmt.setLong(4, alias.time_add.getTime());
-        stmt.executeUpdate();
+    	statement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+    	statement.setInt(1, alias.client_id);
+    	statement.setString(2, alias.name);
+    	statement.setInt(3, alias.num_used);
+    	statement.setLong(4, alias.time_add.getTime());
+    	statement.executeUpdate();
     	
-        // Getting the auto-incremented client id
-    	rs = stmt.getGeneratedKeys();
-    	alias.id = rs.getInt(1);
+    	// Storing the new generated alias id
+    	resultset = statement.getGeneratedKeys();
+    	alias.id = resultset.getInt(1);
         
-    	// Closing current ResultSet and Prepared Statement
-        if (rs != null) rs.close();
-        if (stmt != null) stmt.close();
+    	resultset.close();
+        statement.close();
             
     }
     
@@ -107,24 +103,23 @@ public class AliasDAO {
      * Update domain object in the database.
      * 
      * @author Mathias Van Malderen, Daniele Pantaleone
+     * @param  alias The alias whose informations needs to be updated
      * @throws ClassNotFoundException
      * @throws SQLException 
      **/
     public static void update(Alias alias) throws ClassNotFoundException, SQLException {
         
-    	if (conn == null || conn.isClosed()) 
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 
+    		connection = DataSourceManager.getConnection();
     
-    	stmt = conn.prepareStatement(UPDATE);
-    	stmt.setInt(1, alias.client_id);
-    	stmt.setString(2, alias.name);
-    	stmt.setInt(3, alias.num_used);
-    	stmt.setLong(4, alias.time_edit.getTime());
-    	stmt.setInt(5, alias.id);
-    	stmt.executeUpdate();
-    	
-    	// Closing current Prepared Statement
-        if (stmt != null) stmt.close();
+    	statement = connection.prepareStatement(UPDATE);
+    	statement.setInt(1, alias.client_id);
+    	statement.setString(2, alias.name);
+    	statement.setInt(3, alias.num_used);
+    	statement.setLong(4, alias.time_edit.getTime());
+    	statement.setInt(5, alias.id);
+    	statement.executeUpdate();
+    	statement.close();
         
     }
     
@@ -133,20 +128,19 @@ public class AliasDAO {
      * Delete domain object from the database.
      * 
      * @author Mathias Van Malderen, Daniele Pantaleone
+     * @param  alias The alias whose informations needs to be deleted
      * @throws ClassNotFoundException 
      * @throws SQLException 
      **/
     public static void delete(Alias alias) throws ClassNotFoundException, SQLException {
         
-    	if (conn == null || conn.isClosed()) 	
-    		conn = DataSourceManager.getConnection();
+    	if (connection == null || connection.isClosed()) 	
+    		connection = DataSourceManager.getConnection();
         
-        stmt = conn.prepareStatement(DELETE);
-        stmt.setInt(1, alias.id);
-        stmt.executeUpdate();
-        
-        // Closing current Prepared Statement
-        if (stmt != null) stmt.close();
+    	statement = connection.prepareStatement(DELETE);
+    	statement.setInt(1, alias.id);
+    	statement.executeUpdate();
+    	statement.close();
         
     }
     
