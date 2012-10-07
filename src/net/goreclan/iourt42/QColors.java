fe859e4c2a73f0df9d6683ef83eb6b3da5b81226
Fenix986 <danielepantaleone@me.com>
2012-10-08 00:49:20 +0200

    - fixed syntax for a better view

--- src/net/goreclan/iourt42/QColors.java
+++ src/net/goreclan/iourt42/QColors.java
@@ -11,13 +11,13 @@ package net.goreclan.iourt42;
 
 public class QColors {
 	
-	public static final String RED = "^1";
-	public static final String GREEN = "^2";
-	public static final String YELLOW = "^3";
-	public static final String BLUE = "^4";
-	public static final String CYAN = "^5";
-	public static final String MAGENTA = "^6";
-	public static final String WHITE = "^7";
-	public static final String BLACK = "^8";
+	public static final String RED 		= "^1";
+	public static final String GREEN 	= "^2";
+	public static final String YELLOW 	= "^3";
+	public static final String BLUE 	= "^4";
+	public static final String CYAN 	= "^5";
+	public static final String MAGENTA 	= "^6";
+	public static final String WHITE 	= "^7";
+	public static final String BLACK 	= "^8";
 
 }
