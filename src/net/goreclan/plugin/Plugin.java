188b8acb6bf4de05fecfcedd6c6cabd767b532c8
Fenix986 <danielepantaleone@me.com>
2012-10-15 04:19:10 +0200

    - major changes to abstract Plugin class

--- src/net/goreclan/plugin/Plugin.java
+++ src/net/goreclan/plugin/Plugin.java
@@ -9,27 +9,23 @@
 
 package net.goreclan.plugin;
 
-import net.goreclan.bot.Command;
 import net.goreclan.console.Console;
 import net.goreclan.domain.Client;
 import net.goreclan.domain.Group;
-import net.goreclan.logger.Log;
+import net.goreclan.exception.CommandRegisterException;
 import net.goreclan.parser.XmlConfigParser;
+import net.goreclan.utility.Command;
+import net.goreclan.utility.CommandList;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.apache.log4j.Logger;
 
 public abstract class Plugin {
 	
 	protected final Console console;
+	protected final Logger log;
 	protected final XmlConfigParser config;
-	protected final Log log;
-
-	protected Map<String, Command> commands;
+	
+	protected CommandList commands;
 	
 	protected boolean enabled = true;
 	
@@ -38,23 +34,37 @@ public abstract class Plugin {
 	 * Object constructor.
 	 * 
 	 * @author Daniele Pantaleone
+	 * @param  console A reference to the main BOT console
+	 * @param  log A reference to the main BOT logger
+	 * @param  conf The plugin configuration file name
 	 * @return Plugin
 	 **/
-	public Plugin(Console console, Log log, String conf) {
-		this.console = console;								// Copying console reference.
-		this.log = log;										// Copying main log reference.
-		this.config = new XmlConfigParser(conf);			// Creating a new parser for the plugin configuration file.
-		this.commands = new HashMap<String, Command>();		// Initializing the commands hash map in order to register commands.
-	}
+	public Plugin(Console console, Logger log, String conf) {
+		this.console = console;						// Copying console reference.
+		this.log = log;								// Copying main log reference.
+		this.config = new XmlConfigParser(conf);	// Creating a new parser for the plugin configuration file.
+		this.commands = new CommandList();			// Creating a new CommandList object for future command registering.
+	}	
 	
 	
 	/**
 	 * Load the plugin configuration file.
-	 * Must be implemented in all the plugins.
+	 * Use this function to load plugin specific attributes configuration.
 	 *  
 	 * @author Daniele Pantaleone
 	 **/
 	public abstract void onLoadConfig();
+	
+	
+	/**
+	 * Start the plugin.
+	 * This method is called right after the onLoadConfig() one.
+	 * Use this function to register runtime commands and perform
+	 * operations which needs to be executed at plugin startup.
+	 *  
+	 * @author Daniele Pantaleone
+	 **/
+	public abstract void onStartup();
 	
 	
 	/**
@@ -89,49 +99,14 @@ public abstract class Plugin {
 	
 	
 	/**
-	 * Register a command.
+	 * Return the Command associated to the given name/alias.
 	 * 
 	 * @author Daniele Pantaleone
-	 * @throws SecurityException 
-	 * @throws NoSuchMethodException 
-	 **/	
-	public void addCommand(String cmdName, String cmdAlias, Group minGroup, Group maxGroup) throws NoSuchMethodException, SecurityException {
-		
-		// Converting command name/alias to lowercase.
-		// We are not going to accept commands with uppercase chars
-		// because this will result in a failure while registering commands
-		// having the same name, but using different character cases.
-		cmdName = cmdName.toLowerCase();
-		cmdAlias = cmdName.toLowerCase();
-		
-		String cmdMethod = "Cmd" + Character.toUpperCase(cmdName.charAt(0)) + cmdName.substring(1);
-		Command cmd = new Command(cmdName, cmdAlias, minGroup, maxGroup, this.getClass().getMethod(cmdMethod, Client.class, String.class));
-		this.log.debug("Registering command: " + cmd.toString() + ".");
-		
-	}
-	
-	
-	/**
-	 * Tells whether the Plugin registered the given command.
-	 * 
-	 * @author Daniele Pantaleone
-	 * @param  cmdName The command name
-	 * @return boolean
-	 **/
-	public boolean hasCommand(String cmdName) {
-		return this.commands.containsKey(cmdName);
-	}
-	
-	
-	/**
-	 * Return the Command associated to the given string.
-	 * 
-	 * @author Daniele Pantaleone
-	 * @param  cmdName The command name
+	 * @param  name The command name
 	 * @return Command
 	 */
-	public Command getCommand(String cmdName) {
-		return this.commands.get(cmdName);
+	public Command getCommand(String name) {
+		return this.commands.get(name);
 	}
 	
 	
@@ -141,39 +116,57 @@ public abstract class Plugin {
 	 * @author Daniele Pantaleone
 	 * @return Map<String, Command>
 	 */
-	public Map<String, Command> getCommandList() {
+	public CommandList getCommandList() {
 		return this.commands;
 	}
 	
 	
 	/**
-	 * Create a new object according to the specified Plugin name.
-	 * Invoke the constructor and return back the initialized object.
+	 * Tells whether the Plugin registered the given command name/alias.
 	 * 
 	 * @author Daniele Pantaleone
-	 * @param  name The name of the plugin
-	 * @return Plugin
+	 * @param  name The command name
+	 * @return boolean
 	 **/
-	public static Plugin getPlugin(IrcBot bot, Log log, String pluginName, String pluginConf) {
+	public boolean hasRegisteredCommand(String name) {
+		return this.commands.containsKey(name);
+	}
+	
+	
+	/**
+	 * Register a command.
+	 * 
+	 * @author Daniele Pantaleone
+	 * @throws SecurityException 
+	 * @throws NoSuchMethodException 
+	 **/	
+	protected void registerCommand(String cmdName, String cmdAlias, Group minGroup, Group maxGroup) throws NoSuchMethodException, SecurityException {
 		
 		try {
 			
-			// Getting the correct plugin class in order to instantiate an object and call it's constructor. 
-			// In this way the plugin can perform all the operations needed since we are passing to the plugin
-			// constructor his XML configuration file name (actually we are passing the whole path ^^).
-			Class<?> pluginClass = Class.forName("net.goreclan.plugin." + pluginName);
-			Constructor<?> construct = pluginClass.getConstructor(IrcBot.class, Log.class, String.class);
-			log.debug("Loading plugin '" + pluginName.substring(0, pluginName.length() - 6) + "': " + pluginClass.getName() + "...");
+			// We are going to register command using only lower case characters.
+			// If we let plugin coders registering commands with mixed upper/lower case characters
+			// we may have some problems while checking a command name/alias existing map.
+			// Instead of bothering us with futile errors, we are going to convert the given
+			// command names into lower case strings.
+			cmdName = cmdName.toLowerCase();
+			cmdAlias = cmdName.toLowerCase();
 			
-			Object plugin = construct.newInstance(bot, log, pluginConf);
-			plugin.getClass().getMethod("onLoadConfig").invoke(plugin);
-			log.debug("Plugin '" + pluginName.substring(0, pluginName.length() - 6) + "' loaded: " + pluginClass.getName() + ".");
+			// We need to match the given command name with a specific plugin method.
+			// Given the command name "blub", a map will be created against a Method
+			// named "CmdBlub" which accept as input parameters the "client" object (which refers
+			// to the client who issued the command) and an optional "data" string (may be null) which
+			// contains Method specific extra data, needed to perform certain operations.
+			String cmdMethod = "Cmd" + Character.toUpperCase(cmdName.charAt(0)) + cmdName.substring(1);
+			Command cmd = new Command(cmdName, cmdAlias, minGroup, maxGroup, this.getClass().getMethod(cmdMethod, Client.class, String.class));
+			this.log.debug("Registering command: " + cmd.toString() + ".");
 			
-			return (Plugin)plugin;
-		
-		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			log.error(e.getMessage());
-			return null;
+			// Adding the command to plugin command list.
+			this.commands.put(cmdName, cmdAlias, cmd);
+			
+		} catch (CommandRegisterException e) {
+			// Logging the exception.
+			this.log.error(e.getMessage());
 		}
 		
 	}
